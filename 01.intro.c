30eca7913c13c8d9a5f03e875bf77f74d9ecd696
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2024-07-24 17:05:29 +0530

    Create 01Intro.c

---
+++ 01.intro.c
@@ -0,0 +1,21 @@
+// Write a C program to Print an Integer collected from the user.
+
+#include <stdio.h>
+
+int main() { 
+
+int number;
+
+printf("Enter an integer:\t "); 
+
+// reads and stores input
+
+scanf("%d", &number);
+
+// displays output
+
+printf(“\nYou entered:\t %d", number);
+
+return 0;
+
+}
